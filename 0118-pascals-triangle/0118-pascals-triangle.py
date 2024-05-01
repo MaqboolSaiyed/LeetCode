5fea8765e9cd6d5283d30f5034c334de704235ba
MaqboolSaiyed <64077208+MaqboolSaiyed@users.noreply.github.com>
2024-05-01 20:09:40 +0530

    Time: 46 ms (6.65%), Space: 16.5 MB (58.00%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for i in range(numRows):
+            row = [1]
+            if triangle:
+                last_row = triangle[-1]
+                row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+                row.append(1)
+            triangle.append(row)
+        return triangle
