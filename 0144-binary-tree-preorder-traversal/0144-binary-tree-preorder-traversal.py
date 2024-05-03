cc732614d3a54bcafaacee73ecb73df3ed68b859
Maqbool Saiyed <maqboolsaiyed.rizwan@gmail.com>
2024-05-03 22:25:20 +0530

    Time: 32 ms (76.00%), Space: 16.5 MB (80.45%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+
+        def preorder(node):
+            if node:
+                result.append(node.val)
+                preorder(node.left)
+                preorder(node.right)
+
+        preorder(root)
+        return result
